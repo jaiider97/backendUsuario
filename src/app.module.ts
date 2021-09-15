c656bf3fdf0bded0df0c5f3bcf4c2e56b62680a5
jaider97 <jprogramador97@gmail.com>
2021-09-15 17:13:49 -0500

    tokengenerated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,11 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+require('dotenv').config();
 
-console.log(process.env.JWT_SECRET)
+
+console.log("variable global",process.env.JWT_SECRET);
+console.log("variable global",process.env.HOST);
 
 @Module({
   imports: [
@@ -22,7 +25,10 @@ console.log(process.env.JWT_SECRET)
       retryDelay: 3000
 
     }),
-  ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    
   UsuarioModule,
   AuthModule],
   controllers: [],
--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -5,10 +5,12 @@ export class JwtStrategy extends PassportStrategy (Strategy) {
 
 constructor(){
     super({
-        jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken()
+        jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: process.env.JWT_SECRET
     })
     
 }
+
 
 
 
