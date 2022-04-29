3b6d51002309491cfaa6c474f6d9afa3327ee63d
jaider perez <j97dev@gmail.com>
2022-04-29 13:32:57 -0500

    mod para prueba

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.enableCors();
   const port=process.env.HOST;
   await app.listen(port);
- console.log(await app.getUrl,"___> started");
+ console.log(await app.getUrl,"_mod__> started");
 }
 bootstrap();
